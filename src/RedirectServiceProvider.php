fcdbaf76e64fb45598133e24290f62c5e4ae16ba
Simon Bridgewater <simon@webworx.co.uk>
2017-01-12 19:06:46 +0000

    First commit

---
+++ src/RedirectServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sbridgewater\redirector;
+
+use Illuminate\Support\ServiceProvider;
+
+class RedirectServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
